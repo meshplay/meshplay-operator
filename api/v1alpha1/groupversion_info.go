434447a0425adf0c912d39b2863c2d915a5ab186
khulnasoft <ci@khulnasoft.com>
2024-08-26 08:06:26 +0000

    Changes to be committed:

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 KhulnaSoft, Inc.
+Copyright 2024 KhulnaSoft, Ltd.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
